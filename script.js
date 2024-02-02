9b5b8b230e054d1aba3f19d35b9352e2e4e827ed
comfybonfire <comfybonfire@gmail.com>
2024-02-02 04:30:09 +0100

    Create the UI

--- script.js
+++ script.js
@@ -1,47 +1,31 @@
-function random(){
-    let arr = ['rock', 'paper', 'scissors']
-    let randomIndex = Math.floor(Math.random() * arr.length)
-    return arr[randomIndex]
-}
-function singleRound() {
-    let user = prompt("Choose between: rock, paper and scissors!").toLowerCase()
-    let randomChoice = random()
-    if (randomChoice === 'rock' && user === 'paper') {
-        wonCounter = wonCounter + 1;
-        console.log(`${user} beats ${randomChoice}! You Won!`)
-    } else if (randomChoice === 'paper' && user === 'scissors') {
-        wonCounter = wonCounter + 1;
-        console.log(`${user} beats ${randomChoice}! You Won!`)
-    } else if (randomChoice === 'scissors' && user === 'rock') {
-        wonCounter = wonCounter + 1;
-        console.log(`${user} beats ${randomChoice}! You Won!`)
-    } else if (randomChoice === user) {
-        console.log("It\'s a tie!")
-    } else {
-        lossCounter = lossCounter + 1;
-        console.log(`${randomChoice} beats ${user}! You Loss!`)
-    }
-    console.log(`The result is User: ${wonCounter}, Computer: ${lossCounter}!`);
-};
+const buttons = document.querySelectorAll('button');
+const start = document.querySelector('#start');
+let user;
+let options = ['rock', 'paper', 'scissors'];
 
-let wonCounter = 0
-let lossCounter = 0
-
-function endGame() {
-    if (wonCounter > lossCounter) {
-        console.log("Game Over! You Win! :)");
-    } else if (lossCounter > wonCounter) {
-        console.log("Game Over! You Lost! :(");
-    }
+function random() {
+    return (options[Math.floor(Math.random() * options.length)]);
 }
 
-function game() {
-    singleRound();
-    if(wonCounter < 5 && lossCounter < 5){
-        game();
-    } else {
-        endGame();
-    }
+function round() {
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            user = button.id;
+            random()
+            let computer = random();
+            if (computer === user) {
+                alert('Draw');
+            } else if (computer === 'rock' && user === 'scissors') {
+                alert('you lose');
+            } else if (computer === 'paper' && user === 'rock') {
+                alert('you lose');
+            } else if (computer === 'scissors' && user === 'paper') {
+                alert('you lose');
+            } else {
+                alert('you win');
+            }
+        });
+    });
 }
 
-game();
+start.addEventListener('click', round);
